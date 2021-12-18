2eba9bd5bd369e91511016f9a8bf1b888032e0d4
Ritujauni <91397743+Ritujauni@users.noreply.github.com>
2021-12-18 13:44:16 -0500

    function

--- function.sql
+++ function.sql
@@ -13,3 +13,25 @@ END;
 
 select ip_fetch from dual;
 
+--- Male Female Ratio
+
+create or replace function male_female return varchar2
+ is
+ v1 VARCHAR2(15);
+ v2 VARCHAR2(15);
+ mf VARCHAR2(30);
+ begin
+ Select count(*)||' '||decode(SEX,'M','Male','F','Female') into v1 FROM hrms_employee_details
+ where SEX='M'
+ group by decode(SEX,'M','Male','F','Female');
+ Select count(*)||' '||decode(SEX,'M','Male','F','Female') into v2 FROM hrms_employee_details
+ where SEX='F'
+ group by decode(SEX,'M','Male','F','Female');
+  MF := v1 ||' '|| v2;
+ RETURN MF;
+ END;
+/
+
+select male_female as male_female_ratio  from dual;
+
+select count(*)as total_employee from hrms_employee_details;
