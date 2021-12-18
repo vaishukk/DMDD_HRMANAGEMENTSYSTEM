052519a69edc946fda792066b0349b55391d4a77
Ritujauni <91397743+Ritujauni@users.noreply.github.com>
2021-12-17 23:31:59 -0500

    Add files via upload

---
+++ Drop.sql
@@ -0,0 +1,48 @@
+DROP table department CASCADE CONSTRAINTS;
+
+DROP table designation CASCADE CONSTRAINTS;
+
+
+DROP table PROJECTS CASCADE CONSTRAINTS;
+
+
+DROP table ASSET CASCADE CONSTRAINTS;
+
+DROP table skills CASCADE CONSTRAINTS;
+
+
+DROP table hrms_employee_details CASCADE CONSTRAINTS;
+
+
+DROP table hrms_employee_projects CASCADE CONSTRAINTS;
+
+
+DROP table hrms_employee_salary CASCADE CONSTRAINTS;
+
+
+DROP table hrms_employee_asset CASCADE CONSTRAINTS;
+
+
+
+DROP table hrms_employee_trainings CASCADE CONSTRAINTS;
+
+
+
+
+
+DROP table hrms_employee_leaves CASCADE CONSTRAINTS;
+
+
+DROP table hrms_employee_attendance CASCADE CONSTRAINTS;
+
+
+
+DROP table hrms_kra_kpi CASCADE CONSTRAINTS;
+
+
+DROP table WAGES CASCADE CONSTRAINTS;
+
+    
+
+
+
