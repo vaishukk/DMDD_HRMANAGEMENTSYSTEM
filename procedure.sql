63fc87d460076e268658f33331e970f36696d814
Ritujauni <91397743+Ritujauni@users.noreply.github.com>
2021-12-18 13:47:11 -0500

    updated

--- procedure.sql
+++ procedure.sql
@@ -1,14 +1,28 @@
-create or replace procedure get_pf_bonus
+set serverout on;
+ create or replace procedure get_pf_bonus
 (       xemp_id in number,
 pf out number,
 bonus out number)  is
 begin
- select (annual_incomde*.4)/12,(annual_incomde*.4)/8.33  into pf,bonus from hrms_employee_salary
-       where empid=xemp_id;
+ select (ANNUAL_INCOME*.4)/12,(ANNUAL_INCOME*.4)/8.33  into pf,bonus
+from hrms_employee_salary
+where emp_id=xemp_id;
   EXCEPTION
       WHEN NO_DATA_FOUND THEN
  null;
 end get_pf_bonus;
 /
 
+DECLARE
+ EMPNO NUMBER;
+ PF number;
+ BONUS number;
 
+ BEGIN
+   EMPNO := 1100;
+   PF := 200;
+   BONUS:= 100;
+
+   get_pf_bonus(EMPNO,PF,BONUS );
+Dbms_Output.Put_Line(empno||' PF :'||' '||PF||' BONUS: '||BONUS||' ' );
+ END;
